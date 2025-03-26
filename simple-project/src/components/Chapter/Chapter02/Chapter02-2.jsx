14c2382fa08c878f9d6565a84db1f062db8e85ea
ggkama <ggkama@naver.com>
2025-03-26 17:47:48 +0900

    20250326 정리주석 추가

--- simple-project/src/components/Chapter/Chapter02/Chapter02-2.jsx
+++ simple-project/src/components/Chapter/Chapter02/Chapter02-2.jsx
@@ -1,5 +1,5 @@
 const ChildComponent = (props) => {
-    console.log(props);
+    // console.log(props);
     return (
         <>
             <p>조심할점</p>
@@ -19,7 +19,11 @@ const Chapter022 = () => {
         절대로 props값을 변경해서는 안돼~!
 
         ** 정리 **
-        React의 사용 목적 : UI(User Interface)
+        React의 사용 목적 : 웹 어플리케이션의 UI(User Interface) => MVC의 V 파트
+                                                                  -> 필요한 값 입력받기
+                                                                     요청 보내기
+                                                                     요청 결과 출력
+                            만들 용도로 사용
 
     */
     return (
